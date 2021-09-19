e9dd6cc7b96084acc9edd1e52dd53c330de68999
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-19 10:35:16 -0300

    feat: add example of user

--- api/handlers_user.go
+++ api/handlers_user.go
@@ -1,14 +1,43 @@
 package api
 
 import (
-	"finbuild/entity/user"
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"finbuild/entity/user"
 )
 
 // getUser return api response with user info
 func getUser(w http.ResponseWriter, r *http.Request) {
 
-	user, _ := user.GetUser()
-	apiResponse(user, w)
-}
+	var msg []map[string]interface{}
 
+	u := user.User{
+		ID:         "1",
+		FistName:   "Tonnytg",
+		LastName:   "TG",
+		SocialID:   "001.001.001-01",
+		Age:        50,
+		Email:      "tonnytg@domain.com",
+		ValidEmail: true,
+		Phone:      "+5522999999999",
+		Sex:        "male",
+		Sign:       "Aquarius",
+		Address:    "São Paulo, Brazil",
+	}
+
+	mp1 := map[string]interface{}{
+		"message": "success",
+		"user":    u,
+	}
+	msg = append(msg, mp1)
+
+	b, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+}
--- entity/user/user.go
+++ entity/user/user.go
@@ -4,12 +4,12 @@ type User struct {
 	FistName   string `json:"fist_name"`
 	LastName   string `json:"last_name"`
 	ID         string `json:"id"`
-	SocialID   string `json:"social_id"`	// CPF
+	SocialID   string `json:"social_id"` // CPF
 	Age        int    `json:"age"`
 	Address    string `json:"address"`
 	Sex        string `json:"sex"`
-	Sign       string `json:"sign"` 		// Example: Aquarius
+	Sign       string `json:"sign"` // Example: Aquarius
 	Email      string `json:"email"`
-	ValidEmail string `json:"valid_email"`	// true, valid email
+	ValidEmail bool   `json:"valid_email"` // true, valid email
 	Phone      string `json:"phone"`
 }
