8a44d2e09eea459711ce9d5f06f811467ff08432
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-19 10:33:03 -0300

    fix: adjust new types

--- entity/finance/finance.go
+++ entity/finance/finance.go
@@ -2,23 +2,23 @@ package finance
 
 // Asset it's a term for all kind of item in Finance Market
 type Asset struct {
-	ID       string
-	Price    float64
-	Quantity float64
-	Date     string
-	Action   string // BUY or SELL
+	ID       string  `json:"id"`
+	Price    float64 `json:"price"`
+	Quantity float64 `json:"quantity"`
+	Date     string  `json:"date"`
+	Action   string  `json:"action"` // BUY or SELL
 }
 
 // AssetRent it's all transaction/action gain or lost money
 type AssetRent struct {
-	Date    string
-	Price   float64
-	AssetID string
-	Type    string
-	Percent float64
+	Date    string  `json:"date"`
+	Price   float64 `json:"price"`
+	AssetID string  `json:"assetId"`
+	Type    string  `json:"type"`
+	Percent float64 `json:"percent"`
 }
 
 // Account contains the sum of all assets
 type Account struct {
-	Balance float64
+	Balance float64 `json:"balance"`
 }
