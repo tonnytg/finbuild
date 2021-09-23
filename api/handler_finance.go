1f617a3604285a655965fc0d65e9532f9d82d76c
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-23 08:23:48 -0300

    feat: add exchange reader

--- api/handler_finance.go
+++ api/handler_finance.go
@@ -1,1 +1,51 @@
 package api
+
+import (
+	"encoding/json"
+	"finbuild/entity/finance"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// postExchange store in database action of exchange
+func postExchange(w http.ResponseWriter, r *http.Request) {
+
+	// read body response
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("ERROR: problem to read body", err)
+	}
+
+	// convert body response to struct
+	var f finance.Asset
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		log.Println("ERROR: problem to unmarshal body", err)
+	}
+
+	// create a slice of interface to receive json content
+	mp1 := map[string]interface{}{
+		"exchange": f,
+	}
+
+	// create a map for json template return
+	var msg []map[string]interface{}
+	msg = append(msg, mp1)
+
+	// json template to return
+	jSend := Response{
+		Status:  "success",
+		Data:    msg,
+		Message: "test",
+	}
+
+	b, err := json.Marshal(jSend)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+}
