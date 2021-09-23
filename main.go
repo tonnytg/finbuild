abe74fea43ec1db6d3b81324d3f57be7d33457fe
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-22 23:32:56 -0300

    feat: add db connector

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"finbuild/api"
+	"finbuild/database"
 )
 
 func main() {
+
+	// Start database connection
+	database.InitDatabase()
 
 	// api.StartAPI run API Listen
 	api.StartAPI()
