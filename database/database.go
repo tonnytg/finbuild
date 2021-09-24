ae426aa43514c9737bb735d2e02bebb8d3e0685d
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-24 08:59:50 -0300

    feat: add db fields for finance fields

--- database/database.go
+++ database/database.go
@@ -8,9 +8,10 @@ import (
 
 var DB *gorm.DB
 
+// InitDatabase start database with QueryField allow get one or more fields
 func InitDatabase() {
 	dsn := "host=127.0.0.1 user=postgres password=postgres dbname=finbuild port=5432 sslmode=disable"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{QueryFields: true})
 	if err != nil {
 		log.Println("ERROR: problem opening database", err)
 	}
--- entity/finance/finance.go
+++ entity/finance/finance.go
@@ -1,24 +1,49 @@
 package finance
 
+import (
+	"finbuild/database"
+	"github.com/google/uuid"
+)
+
+type AssetInterface interface {
+	UpdateBalance() (float64, error)
+}
+
 // Asset it's a term for all kind of item in Finance Market
 type Asset struct {
-	ID       string  `json:"id"`
-	Price    float64 `json:"price"`
-	Quantity float64 `json:"quantity"`
-	Action   string  `json:"action"` // BUY or SELL
-	Date     string  `json:"date"`
+	UserID   uuid.UUID `json:"user_id"`
+	ID       string    `json:"id"`
+	Price    float64   `json:"price"`
+	Quantity float64   `json:"quantity"`
+	Action   string    `json:"action"` // BUY or SELL
+	Date     string    `json:"date"`
 }
 
 // AssetRent it's all transaction/action gain or lost money
 type AssetRent struct {
-	Date    string  `json:"date"`
-	Price   float64 `json:"price"`
-	AssetID string  `json:"assetId"`
-	Type    string  `json:"type"`
-	Percent float64 `json:"percent"`
+	UserID  uuid.UUID `json:"user_id"`
+	Date    string    `json:"date"`
+	Price   float64   `json:"price"`
+	AssetID string    `json:"assetId"`
+	Type    string    `json:"type"`
+	Percent float64   `json:"percent"`
 }
 
 // Account contains the sum of all assets
 type Account struct {
-	Balance float64 `json:"balance"`
+	UserID  uuid.UUID `json:"user_id"`
+	Balance float64   `json:"balance"`
+}
+
+func (a *Account) UpdateBalance(userid uuid.UUID, price float64) (float64, error){
+
+	// get balance
+	var account Account
+	database.DB.Table("accounts").First(&account).Scan(&account)
+
+	// update balance
+	v := account.Balance + price
+	database.DB.Model(&Account{}).Where("user_id = ?", userid).Update("balance", v)
+
+	return v, nil
 }
