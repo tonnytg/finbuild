d3048b0e3333b2d12adb25031ea7dd5ab75cec46
Antonio Thomacelli <tonnytg@gmail.com>
2021-09-24 09:00:52 -0300

    fix: add new field

--- entity/user/user.go
+++ entity/user/user.go
@@ -1,15 +1,17 @@
 package user
 
+import "github.com/google/uuid"
+
 type User struct {
-	ID         string `json:"id"`
-	FirstName  string `json:"first_name" db:"first_name"`
-	LastName   string `json:"last_name"`
-	SocialID   string `json:"social_id"` // CPF
-	Phone      string `json:"phone"`
-	Email      string `json:"email"`
-	ValidEmail bool   `json:"valid_email"` // true, valid email
-	Address    string `json:"address,omitempty"`
-	Age        int    `json:"age,omitempty"`
-	Sex        string `json:"sex,omitempty"`
-	Sign       string `json:"sign,omitempty"` // Example: Aquarius
+	ID         uuid.UUID `json:"id"`
+	FirstName  string    `json:"first_name" db:"first_name"`
+	LastName   string    `json:"last_name"`
+	SocialID   string    `json:"social_id"` // CPF
+	Phone      string    `json:"phone"`
+	Email      string    `json:"email"`
+	ValidEmail bool      `json:"valid_email"` // true, valid email
+	Address    string    `json:"address,omitempty"`
+	Age        int       `json:"age,omitempty"`
+	Sex        string    `json:"sex,omitempty"`
+	Sign       string    `json:"sign,omitempty"` // Example: Aquarius
 }
