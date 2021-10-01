53b46ad6b07932008d8d10f694ad44a817c258e1
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-01 19:05:07 -0300

    fix: problem circular dependencies

--- database/finance.go
+++ database/finance.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"database/sql"
-	"finbuild/entity/finance"
 )
 
 type FinanceDb struct {
@@ -13,17 +12,3 @@ func NewFinanceDb(db *sql.DB) *FinanceDb {
 	return &FinanceDb{db: db}
 }
 
-func (f *FinanceDb) Get(id string) (finance.AssetInterface, error) {
-	var asset finance.Asset
-	stmt, err := f.db.Prepare("select user_id, id, price, quantity, action, date from assets where user_id=$1")
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-
-	err = stmt.QueryRow(id).Scan(&asset.UserID, &asset.ID, &asset.Price, &asset.Quantity, &asset.Action, &asset.Date)
-	if err != nil {
-		return nil, err
-	}
-	return &asset, nil
-}
