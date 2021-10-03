2bbadfce183f2eb6ea30ab9d963cdd15b2d7fdcf
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-03 11:15:39 -0300

    fix: refact to short name

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -1,4 +1,4 @@
-package database
+package db
 
 import (
 	log "finbuild/pkg/logging"
@@ -9,12 +9,12 @@ import (
 
 var DB *gorm.DB
 
-// InitDatabase start database with QueryField allow get one or more fields
+// InitDatabase start db with QueryField allow get one or more fields
 func InitDatabase() {
 	dsn := "host=127.0.0.1 user=postgres password=postgres dbname=finbuild port=5432 sslmode=disable"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{QueryFields: true})
 	if err != nil {
-		msg := fmt.Sprintf("error to connect database: %v", err)
+		msg := fmt.Sprintf("error to connect db: %v", err)
 		log.Msg("CRITICAL", msg)
 	}
 	DB = database
