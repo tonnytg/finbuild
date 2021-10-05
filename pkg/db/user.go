dbbed7ee42a61168852521deadad2e631eceda75
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-05 10:22:37 -0300

    feat: create interface with db and add getshortuser

--- pkg/db/user.go
+++ pkg/db/user.go
@@ -1,1 +1,31 @@
 package db
+
+import (
+	"finbuild/entity/finance"
+	"finbuild/entity/user"
+	"github.com/google/uuid"
+)
+
+func UserRegistry(user *user.User, wallet *finance.Wallet) {
+
+	DB.Model(&user).Create(&user)
+
+	DB.Table("wallets").Model(&finance.Wallet{}).Create(&wallet)
+
+}
+
+// GetUser get all information from users where userID
+func GetUser(u uuid.UUID) *user.User {
+
+	uu := user.User{}
+	DB.First(&uu, u).Scan(&uu)
+	return &uu
+}
+
+// GetShortUser get short information from users where userID
+func GetShortUser(u uuid.UUID) *user.ShortUsers {
+
+	su := user.ShortUsers{}
+	DB.Table("users").First(&su, "user_id = ?", u).Scan(&su).Find(&user.ShortUsers{})
+	return &su
+}
