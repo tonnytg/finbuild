341deee3bec4059c5b3a1d5b2e57e2a234c6c5fc
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-05 10:12:29 -0300

    fix: rename entity to finance

--- pkg/db/finance.go
+++ pkg/db/finance.go
@@ -2,7 +2,8 @@ package db
 
 import (
 	"errors"
-	entity "finbuild/entity/finance"
+	"finbuild/entity/finance"
+	"github.com/google/uuid"
 
 	log "finbuild/pkg/logging"
 	"fmt"
@@ -11,19 +12,19 @@ import (
 func UpdateBalance(WalletID string, Action string, Quantity float64, Price float64, Tax float64, Date string) (float64, error) {
 
 	// get balance
-	var account entity.Wallet
+	var account finance.Wallet
 	DB.Table("wallets").First(&account).Scan(&account)
 
 	if Action == "BUY" {
 		// update balance
 		v := account.Balance + (Price * Quantity)
-		DB.Table("wallets").Model(&entity.Wallet{}).Where("wallet_id = ?", WalletID).Update("balance", v)
+		DB.Table("wallets").Model(&finance.Wallet{}).Where("wallet_id = ?", WalletID).Update("balance", v)
 		return v, nil
 	}
 	if Action == "SELL" {
 		// update balance
 		v := account.Balance - (Price * Quantity)
-		DB.Table("wallets").Model(&entity.Wallet{}).Where("wallet_id = ?", WalletID).Update("balance", v)
+		DB.Table("wallets").Model(&finance.Wallet{}).Where("wallet_id = ?", WalletID).Update("balance", v)
 		return v, nil
 	}
 
@@ -33,7 +34,7 @@ func UpdateBalance(WalletID string, Action string, Quantity float64, Price float
 func RegisterExchange(WalletID string, AssetID string, Action string, Tax float64, Quantity float64, Price float64, Date string) error {
 	msg := fmt.Sprintf("save in db - wallet_id: %s asset_id: %s action: %s quantity: %f price: %f", WalletID, AssetID, Action, Quantity, Price)
 	log.Msg("INFO", msg)
-	as := &entity.Exchanges{
+	as := &finance.Exchanges{
 		WalletID: WalletID,
 		AssetID:  AssetID,
 		Price:    Price,
@@ -42,6 +43,11 @@ func RegisterExchange(WalletID string, AssetID string, Action string, Tax float6
 		Action:   Action,
 		Date:     Date,
 	}
-	DB.Table("exchanges").Model(&entity.Exchanges{}).Create(&as)
+	DB.Table("exchanges").Model(&finance.Exchanges{}).Create(&as)
 	return nil
+}
+
+func GetWallets(shortWallet *finance.ShortWallet, u uuid.UUID) *finance.ShortWallet{
+	DB.Table("wallets").First(&shortWallet, "user_id = ?", u).Scan(&shortWallet).Find(&finance.Wallet{})
+	return shortWallet
 }
