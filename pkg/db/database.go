021700593a7fb92f7024ca46e88be0cbdbdc7522
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-23 23:38:54 -0300

    feat: add env database

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -5,13 +5,29 @@ import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 
 // InitDatabase start db with QueryField allow get one or more fields
 func InitDatabase() {
-	dsn := "host=127.0.0.1 user=postgres password=postgres dbname=finbuild port=5432 sslmode=disable"
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbSSL := os.Getenv("DB_SSL")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		dbHost,
+		dbUser,
+		dbPassword,
+		dbName,
+		dbPort,
+		dbSSL,
+	)
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{QueryFields: true})
 	if err != nil {
 		msg := fmt.Sprintf("error to connect db: %v", err)
